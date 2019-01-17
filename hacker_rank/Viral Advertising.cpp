5b8acaca25e199eac053045e9aed2053c317d4ed
Anu1601CS <anuragvns1111@gmail.com>
2019-01-17 11:55:48 +0530

    new

---
+++ hacker_rank/Viral Advertising.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+#define SYNC                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+#define inf 0x3f3f3f3f
+#define INF 1000111000111000111LL
+#define mod 1000000007
+#define pi acos(-1.0)
+#define eps 1e-8
+#define endl '\n'
+#define mp make_pair
+#define mt make_tuple
+#define pb push_back
+#define fi first
+#define se second
+
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<bool> vb;
+typedef vector<vector<int>> vvi;
+typedef vector<long long int> vlli;
+typedef vector<pair<int, int>> vpii;
+
+int main()
+{
+    SYNC int n;
+    cin >> n;
+
+    int res = 0, like = 0, bn = 5;
+    for (int i = 0; i < n; i++)
+    {
+        like = floor(bn/2);
+        res += like;
+        bn = like * 3;
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
